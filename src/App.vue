8753e422d35a5293b1c0f447981039cc7d7a76c2
Sergey <memonanofsa@gmail.com>
2019-04-21 11:35:52 +0500

    Add btn component and make first step

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,54 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="container__wrap">
+    <div class="form__row">
+      <div class="h2">Where would you like to display your campaign?</div>
+      <ButtonNewRule/>
+    </div>
+
+    <div class="form__row">
+      <div class="h2">Exclusion Rules</div>
+      <ButtonNewRule/>
+    </div>
+    
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import Component, Vue } from 'vue-property-decorator';
+
+import ButtonNewRule from '@/components/ButtonNewRule.vue';
 
 @Component({
   components: {
-    HelloWorld,
+    ButtonNewRule,
   },
 })
-export default class App extends Vue {}
+
+export default class App extends Vue {
+}
 </script>
+
 <style lang="stylus">
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+
+body
+  font-family 'Roboto', sans-serif
+  font-size 16px
+  margin 0
+  color #292929
+
+
+.container__wrap
+  padding 20px 15px
+
+
+.h2
+  margin-bottom 20px
+  font-size 18px
+  font-weight 500
+
+
+.form__row
+  margin-bottom 31px
+
+
 </style>
---
+++ src/components/ButtonNewRule.vue
@@ -0,0 +1,29 @@
+<template>
+  <button class="btn">
+    <span class="plus">+</span>New rule
+  </button>
+</template>
+
+<style lang="stylus" scoped>
+.btn
+  margin-top 4px
+  padding 11px 27px 10px
+  border 2px solid #dddee3
+  border-radius 6px
+  text-transform uppercase
+  font-size 14px
+  font-weight 500
+  &:hover
+    cursor pointer
+    background-color #dddee3
+    .plus
+      color #999999
+
+.plus
+  position relative
+  top -2px
+  padding-right 4px
+  color #c6c6c6
+  font-weight 100
+
+</style>
